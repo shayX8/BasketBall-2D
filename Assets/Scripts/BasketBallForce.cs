fe661cf0d271d7b2cee61b85e5a1a4c1a372add5
shayX8 <shayX8@gmail.com>
2022-06-14 14:05:47 +0300

    unity

---
+++ Assets/Scripts/BasketBallForce.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketBallForce : MonoBehaviour
+{
+    Rigidbody2D basketBallForce;
+    float startXPosition, startYPosition, xPosition, yPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startXPosition = transform.position.x;
+        startYPosition = transform.position.y;
+        
+
+        basketBallForce = GetComponent<Rigidbody2D>();
+        basketBallForce.AddForce(new Vector2(-100, 400));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xPosition = transform.position.x;
+        yPosition = transform.position.y;
+        if (xPosition <8.3f && xPosition > 6.4f && yPosition > -0.2f && yPosition < 0)
+        {
+            //freez ball on the basket
+            //basketBallForce.velocity = new Vector2(0, 0);
+
+            ScoreText.scoreValue++;
+
+            //refresh game after score 
+            transform.position = new Vector2(startXPosition, startYPosition);
+            basketBallForce.AddForce(new Vector2(-100, 400));
+
+            //retorn player to start position
+            PlayerMovement.ResetLocation();
+            
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float  dirHor, dirVer, dirMouseX, dirMouseY;
+    static float startXPosition, startYPosition, xPosition, yPosition;
+    static Transform reLocationTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        reLocationTransform = GetComponent<Transform>();
+        xPosition = transform.position.x;
+        yPosition = transform.position.y;
+        startXPosition = transform.position.x;
+        startYPosition = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //keybord movment
+        dirHor = Input.GetAxisRaw("Horizontal");
+        dirVer = Input.GetAxisRaw("Vertical");
+
+        //left and right
+        xPosition = xPosition + (dirHor / 10 * Time.deltaTime);
+
+        //up and dowe
+        yPosition = yPosition + (dirVer / 20 * Time.deltaTime);
+
+
+        //mouse movment 
+        dirMouseX = Input.GetAxis("Mouse X");
+        dirMouseY = Input.GetAxis("Mouse Y");
+
+        //left and right
+        xPosition = xPosition + (dirMouseX);
+
+        //up and dowe
+        yPosition = yPosition + (dirMouseY);
+
+        //the player dont go out of the borders
+        if (xPosition > 8.1f)
+        {
+            xPosition = 8.1f;
+        }
+        else if (xPosition < -8.1f)
+            xPosition = -8.1f;
+
+        if (yPosition > 4.2f)
+        {
+            yPosition = 4.2f;
+        }
+        else if (yPosition < -4.2f)
+            yPosition = -4.2f;
+
+        transform.position = new Vector2(xPosition, yPosition);
+    }
+
+    //retorn player to start position
+    public static void ResetLocation()
+    {
+        reLocationTransform.transform.position = new Vector2(startXPosition, startYPosition);
+        xPosition = startXPosition;
+        yPosition = startYPosition;
+
+    }
+}
